51915f82b41d62ba205361b872d25f37255b902d
Shehbab-Kakkar <shehbab1@gmail.com>
2025-08-09 00:24:42 +0530

    Create Ownership.rs

---
+++ Rust/Ownership.rs
@@ -0,0 +1,8 @@
+//Ownership Transfer Rule 
+//One one Ownership, each location have a variable
+fn main(){
+    let a:String = String::from("Hello World");
+    let b:String = a;
+    //println!("{}",a);
+    println!("{}",b);
+}
