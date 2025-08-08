9317feb2bbd1bcf79e9c3cc96936dff7f1f2cf90
Shehbab-Kakkar <shehbab1@gmail.com>
2025-08-08 20:50:40 +0530

    Create ifelseif.rs

---
+++ Rust/ifelseif.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let age: i32 = 10;
+    if (age > 50){
+        println!("Time for Retire from job");
+    } else if (age < 50){
+        println!("Age is good for work under 50");
+    } else {
+        println!("age is equal to {age}");
+    }
+}
