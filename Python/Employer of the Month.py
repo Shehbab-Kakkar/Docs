4af977d7de7a2f24f43f6290b51c15236f280194
Shehbab-Kakkar <shehbab1@gmail.com>
2025-08-15 10:58:46 +0530

    Create Employer of the Month.py

---
+++ Python/Employer of the Month.py
@@ -0,0 +1,15 @@
+#Program to get the Employer of the Month
+employee = [("Ram",200),("Mohan",100),("John",50)]
+def get_employee_of_month(employee):
+      WorkingHours = 0
+      empOfMonth =  ''
+      for emp,hours in employee:
+        if hours > WorkingHours:
+          empOfMonth = emp
+          WorkingHours = hours
+          
+        else:
+          pass
+      
+      return (empOfMonth,WorkingHours)        
+print(get_employee_of_month(employee))       
