eebaa97056a8acd5c399072086679e240fa16d55
Shehbab Kakkar <shehbab1@gmail.com>
2025-08-23 15:57:14 +0530

    Update HashMap-Insert-Update.rs

--- Rust/HashMap-Insert-Update.rs
+++ Rust/HashMap-Insert-Update.rs
@@ -8,17 +8,18 @@ fn main() {
     let mut marks: HashMap<&str, i32> = HashMap::new();
     marks.insert("Ram",23);
     marks.insert("Sahil",24);
-    marks.insert("Arjun",25);
-    marks.insert("Rampal",26);
-    marks.insert("Goldy",27);
+    marks.insert("Arjun",25); 
     println!("Print the marks\n{:?}",marks);
     if let Some(mark) = marks.get("Ram") {
         println!("Ram is present his marks : {}",mark);
         println!("Updating Ram value");
         marks.insert("Ram",50);
-        println!("Ram value updated to {}",marks["ram"]);
+        println!("Ram value updated to {}",marks["Ram"]);
         
     } else {
         println!("Ram is not Present")
     }         
+    marks.entry("Jassy").or_insert(40);
+    marks.entry("Arjun").and_modify(|i| *i += 500);
+    println!("{:?}",marks);
 }
