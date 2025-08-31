a11a594b7a22e63a67ebf7a9a5ea1b945ea785dd
Shehbab Kakkar <shehbab1@gmail.com>
2025-08-31 08:00:06 +0530

    Update Nos.Of.CPUs.go

--- Golang/Goroutine/Nos.Of.CPUs.go
+++ Golang/Goroutine/Nos.Of.CPUs.go
@@ -8,5 +8,14 @@ import (
 func main() {
 	fmt.Println("main execution started")
 	fmt.Println("No. of CPUs", runtime.NumCPU())
-
+	fmt.Println("No. of Goroutines:", runtime.NumGoroutine())
+	fmt.Println("OS:", runtime.GOOS)
+	fmt.Println("Arch:", runtime.GOARCH)
 }
+/*
+main execution started
+No. of CPUs 2
+No. of Goroutines: 1
+OS: linux
+Arch: amd64
+*/
