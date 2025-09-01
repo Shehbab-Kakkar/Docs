90741cbddf142ffa81f7c8c313e8b1f8bd41b6af
Shehbab Kakkar <shehbab1@gmail.com>
2025-09-01 23:02:12 +0530

    Create EasyWay.to.ReadFile.go

---
+++ Golang/Files/EasyWay.to.ReadFile.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//read file
+	// f, err := os.Open("testing/example.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+	// fileInfo, err := f.Stat()
+	// if err != nil {
+	// 	// If an error occurs while getting file info, terminate the program
+	// 	panic(err)
+	// }
+
+	// buf := make([]byte, fileInfo.Size())
+	// d, err := f.Read(buf) // read the length of the buffer which is 10
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// // for i := 0; i < len(buf); i++ {
+	// // 	println("data", d, string(buf[i]))
+	// // }
+	data, err := os.ReadFile("testing/example.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
+}
