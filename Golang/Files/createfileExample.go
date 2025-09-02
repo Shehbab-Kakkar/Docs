2c1168d64f12d1d6dc3f9854bf7f6e6e45ee1dc8
Shehbab Kakkar <shehbab1@gmail.com>
2025-09-02 08:45:27 +0530

    Create createfileExample.go

---
+++ Golang/Files/createfileExample.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	f, err := os.Create("testing/example3.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString("First Line\n")
+	f.WriteString("Second Line\n")
+}
