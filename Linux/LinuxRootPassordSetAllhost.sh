f2124ea894dfd2a9f4383ee6c25c89aa107ebcc0
Shehbab Kakkar <shehbab1@gmail.com>
2025-11-17 12:17:14 +0530

    Add hostname and OS version check to root password script

--- Linux/LinuxRootPassordSetAllhost.sh
+++ Linux/LinuxRootPassordSetAllhost.sh
@@ -1,1 +1,2 @@
 for i in `cat hosts`; do sshpass -p 'xxxx!' ssh -o StrictHostKeyChecking=no root@$i "echo 'root:xxxx!' | chpasswd"; done
+for i in `cat hosts`; do sshpass -p 'xxx' ssh -o StrictHostKeyChecking=no root@$i 'hostname;cat /etc/redhat-release;' ;done
